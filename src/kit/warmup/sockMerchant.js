0a8a6935cb0c21a02d63918ef55569a972653a76
latoyaallen <me@tailorlabs.io>
2019-08-26 19:01:30 -0400

    sockMerchant done

--- src/kit/warmup/sockMerchant.js
+++ src/kit/warmup/sockMerchant.js
@@ -1,3 +1,18 @@
+// each num in the array is a sock
+// each matching pair of numbers in the arr is a pair of socks
+// so [10,20,20] has 1 lone sock and 1 pair of matching socks.
+
 export default function sockMerchant(n, arr) {
-  return "fixme";
+  const set = new Set(); // a set cannot have duplicate values, so it's good to use for this sort of problem.
+  let count = 0; // let is block scoped. var is defined throughout the program, so we don't want to use var.
+
+  arr.forEach(function(sock) { //iterate through each item in the array, arr
+    if(set.has(sock)) { // if the sock is already in the set
+      count ++; // increase count by one because we've found a pair
+      set.delete(sock); // remove the sock from the set as a set cannot have duplicate values.
+    } else {
+      set.add(sock); // add the sock to the set
+    }
+  });
+  return count;
 }
--- src/kit/warmup/sockMerchant.test.js
+++ src/kit/warmup/sockMerchant.test.js
@@ -1,5 +1,13 @@
 import sockMerchant from './sockMerchant';
 
 it('gives 0 when no match found', () => {
-  expect(sockMerchant(2, [10, 20])).toEqual(0);
+  expect(sockMerchant(4, [10, 20, 30, 40])).toEqual(0);
+});
+
+it('gives 1 when one match found', () => {
+  expect(sockMerchant(3, [10, 20, 20])).toEqual(1);
+});
+
+it('gives 3 when three  matchs found', () => {
+  expect(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20])).toEqual(3);
 });
