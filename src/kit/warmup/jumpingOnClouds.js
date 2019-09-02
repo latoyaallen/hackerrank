5519d4522b449b9add3e73ecadc5f0eefd160059
latoyaallen <me@tailorlabs.io>
2019-09-02 14:01:52 -0400

    Passing jumpingOnClouds test

--- src/kit/warmup/jumpingOnClouds.js
+++ src/kit/warmup/jumpingOnClouds.js
@@ -6,6 +6,17 @@
 
 //[0 0 1 0 0 1 0] should return 4
 export default function jumpingOnClouds(n, arr) {
-  return "fixme";
+  let numJumps = 0;
+  let index = 0; // starting index
+  while(index < arr.length - 1) { // while the index we are on is less than the last index of the array, arr.length - 1,
+    if(arr[index+2] == 1){ // We can jump up to two spots ahead.  But if there is a 1 in that spot, only jump by 1.
+      index++; // we only jump by 1 because if we jumped by two we'd land on a bad cloud; so increase the index by 1;
+      numJumps++;// we've made a jump, so increase num of jumps by one
+    } else {
+      index = index + 2;// jump forward by two indices because it's a safe cloud
+      numJumps++; // we've made a single jump, so increase num of jumps by one
+    }
+  }
+  return numJumps;
 
 }
