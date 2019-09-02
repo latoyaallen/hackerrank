382870904b3c8549f7c20f0201aa8aefa7b67118
latoyaallen <me@tailorlabs.io>
2019-09-02 16:59:09 -0400

    Refactor Jumping On Clouds

--- src/InterviewPrepKit/Warmup/jumpingOnClouds.js
+++ src/InterviewPrepKit/Warmup/jumpingOnClouds.js
@@ -1,22 +1,37 @@
-// each num in the array is a cloud
-// 0 is a cloud we can jumo on
-// 1 is a clound we cannot jump on
-// we can jump forward from 0 to 0 as long as the cloud we are jumping on has an index of 1 or 2 more than our current index
-// so [0,1,0] two clouds we can jump on.  We can jump from index 0 to index 2.
+const badCloudFound = (arr, index) => arr[index+2] == 1 ;
 
-//[0 0 1 0 0 1 0] should return 4
 export default function jumpingOnClouds(n, arr) {
   let numJumps = 0;
-  let index = 0; // starting index
-  while(index < arr.length - 1) { // while the index we are on is less than the last index of the array, arr.length - 1,
-    if(arr[index+2] == 1){ // We can jump up to two spots ahead.  But if there is a 1 in that spot, only jump by 1.
-      index++; // we only jump by 1 because if we jumped by two we'd land on a bad cloud; so increase the index by 1;
-      numJumps++;// we've made a jump, so increase num of jumps by one
+  let index = 0;
+
+  while(index < arr.length - 1) {
+    if(badCloudFound(arr, index)){
+      index++;
+      numJumps++;
     } else {
-      index = index + 2;// jump forward by two indices because it's a safe cloud
-      numJumps++; // we've made a single jump, so increase num of jumps by one
+      index = index + 2;
+      numJumps++;
     }
   }
   return numJumps;
-
 }
+
+// Problem
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+// Solution
+// WHILE the index we are on is less than the last index of the array, arr.length - 1,
+// IF there is a 1 in that spot, only jump by 1.
+// INCREASE the index by 1 because we want to check the next cloud.
+// INCREASE num of jumps by one b/c we made a jump.
+// } else {
+// INCREASE the index by two because we jump forward by two indices because it's a safe cloud
+// INCREASE num of jumps by one.
+
+// Refactor
+//Although using const to define functions seems like a hack, but it comes with some great advantages that make it superior (in my opinion)
+//It makes the function immutable, so you don't have to worry about that function being changed by some other piece of code.
+//You can use fat arrow syntax, which is shorter & cleaner.
+//Using arrow functions takes care of this binding for you.
+//Source: https://stackoverflow.com/questions/33040703/proper-use-of-const-for-defining-functions-in-javascript
+
