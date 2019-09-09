7fc1ee822d608f2c634774fa1603ca4a752a3c0b
latoyaallen <me@tailorlabs.io>
2019-09-09 07:46:54 -0400

    Finish balanced brackets

--- src/DataStructures/Stacks/balancedBrackets.js
+++ src/DataStructures/Stacks/balancedBrackets.js
@@ -7,15 +7,24 @@ export default function balancedBrackets(s) {
   const stack = [];
   let i = 0;
 
+  const openingBracket = (value) => (value === '{' || value === '[' || value === '(');
+
   while(i < s.length) {
-    if(s[i] === '{' || s[i] === '[' || s[i] === '(') {
+    if(openingBracket(s[i])) {
       stack.push(s[i]);
-    };
-    if(bracketMap.get(s[i])) { // we found a matching closing bracket
-      stack.pop;
-      i++;
-      return "YES";
+    } else {
+      // we found a closing bracket
+      // look back at the last item added to the stack
+      const topOfStack = stack[stack.length - 1];
+      // if it's a closing bracket, return YES, else NO
+      const closeFound = bracketMap.get(topOfStack) === s[i];
+      if(closeFound) {
+        return "YES";
+      } else {
+        return "NO";
+      }
     }
+    i++;
   }
 }
 
@@ -24,23 +33,3 @@ export default function balancedBrackets(s) {
 //  It must return a string: YES if the sequence is balanced or NO if it is not.
 //function has the following parameter(s):
 //s: a string of brackets
-
-// Solution
-// LOOP through the string
-// If the current chr is an opening brace (note: there are three different opening braces in this problem)
-// PUSH that current chr onto the stack
-// ELSE we're going to see a closing brace, so we need to CHECK the top of the stack
-// IF it doesn't have the corresponding opening brace (This could mean it has the wrong brace OR no brace at all)
-// RETURN No
-
-
-// Notes
-// I found this solution easier to find when given a smaller test case, and building up from that
-// In HackerRank, you can upload a custom test case in two lines (notice there are no string quotes,
-// and our function doesn't require a number of inputs)
-// 1
-//{[()]}
-//is the equivalent of
-//const s = "{[()]}"
-// expect(balancedBrackets(s)).toEqual("whatever you are returning");
-//
--- src/DataStructures/Stacks/balancedBrackets.test.js
+++ src/DataStructures/Stacks/balancedBrackets.test.js
@@ -4,3 +4,13 @@ it('gives YES when given a balanced string', () => {
   const s = "{[()]}"
   expect(balancedBrackets(s)).toEqual("YES");
 });
+
+it('gives NO when given a non balanced string', () => {
+  const s = "{[(])}"
+  expect(balancedBrackets(s)).toEqual("NO");
+});
+
+it('gives YES when given a non balanced string', () => {
+  const s = "{{[[(())]]}}"
+  expect(balancedBrackets(s)).toEqual("YES");
+});
